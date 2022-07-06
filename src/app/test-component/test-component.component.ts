b369762192329d24977eff4b712d4244aac2c483
Dudley-Boxista <mvr2892@gmail.com>
2022-07-06 00:56:59 -0300

    Implement two new components

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test-component',
   templateUrl: './test-component.component.html',
   styleUrls: ['./test-component.component.css']
 })
+
 export class TestComponentComponent implements OnInit {
 
-  constructor() { }
+  @Input() gameName: string;
+  @Input() imgUrl: string;
+
+  constructor() {
+    this.gameName = '';
+    this.imgUrl = '';
+  }
 
   ngOnInit(): void {
   }
